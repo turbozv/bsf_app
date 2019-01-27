314dda1c4ce69b68e7e1608c13f4103b2847d667
Wei Zhang <turbozv@gmail.com>
2019-01-26 23:02:38 -0800

    Added icon in lesson list

--- lib/lessons_widget.dart
+++ lib/lessons_widget.dart
@@ -15,24 +15,27 @@ class Lessons extends StatefulWidget {
 }
 
 class _LessonsState extends State<Lessons> {
-  var _items = List<String>();
+  var _booksJson = Map<String, dynamic>();
+  var _bookList = List<dynamic>();
 
   @override
   void initState() {
     super.initState();
 
-    if (_items.length == 0) {
-      parseJsonFromAssets('assets/json/books.json').then((dmap) {
+    if (_booksJson.keys.length == 0) {
+      parseJsonFromAssets('assets/json/books.json').then((books) {
         var items = List<String>();
-        for (var year in dmap['chs']['booklist']) {
-          items.add(year['title']);
+        var lessons = List<String>();
+        for (var year in books['chs']['booklist']) {
+          lessons.add(year['title']);
           for (var week in year['lessons']) {
-            items.add(week['id'] + ': ' + week['name']);
+            items.add(week['id'] + ' ' + week['name']);
           }
         }
 
         setState(() {
-          _items = items;
+          _booksJson = books;
+          _bookList = _booksJson['chs']['booklist'];
         });
       });
     }
@@ -41,11 +44,42 @@ class _LessonsState extends State<Lessons> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: _items.length,
+        itemCount: _bookList.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('${_items[index]}'),
+          var list = List<Widget>();
+          for (var item in _bookList[index]['lessons']) {
+            var data = item['name'].split(' ');
+            list.add(new Container(
+                margin: const EdgeInsets.fromLTRB(15, 2, 5, 2),
+                decoration: new BoxDecoration(
+                    border: new Border.all(color: Colors.grey, width: 0.5),
+                    borderRadius: BorderRadius.circular(10)),
+                child: ListTile(
+                    title: Text('${data[1]}'),
+                    subtitle: Text('${data[2]} ${data[0]}'),
+                    trailing: Image.asset(
+                      'assets/images/Materials.On.png',
+                      width: 34,
+                      height: 34,
+                    ))));
+          }
+          return ExpansionTile(
+            title: Text(_bookList[index]['title']),
+            children: list.toList(),
           );
         });
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return ListView.builder(
+  //       itemCount: _items.length,
+  //       itemBuilder: (context, index) {
+  //         var data = _items[index].split(' ');
+  //         return ListTile(
+  //           title: Text('${data[2]}'),
+  //           subtitle: Text('${data[3]} ${data[1]}'),
+  //         );
+  //       });
+  // }
 }
